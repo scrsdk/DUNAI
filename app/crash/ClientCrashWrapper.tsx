0787d6dd7d88d228942d9297eec19f38c65a851f
KirScr <142673005+scrsdk@users.noreply.github.com>
2025-09-07 23:51:43 +0300

    Update ClientCrashWrapper.tsx

--- app/crash/ClientCrashWrapper.tsx
+++ app/crash/ClientCrashWrapper.tsx
@@ -1,16 +1,1 @@
-  // app/crash/ClientCrashWrapper.tsx
-    "use client"; // Обязательно добавьте эту директиву!
-
-    import dynamic from 'next/dynamic';
-
-    // Импортируем сам игровой компонент.
-    // Убедитесь, что путь к PhaserCrashGame правильный относительно этого нового файла.
-    // Если PhaserCrashGame находится в app/crash/components/crash/, то путь будет ../../components/crash/PhaserCrashGame
-    // Если PhaserCrashGame находится в app/components/crash/, то путь будет ../../components/crash/PhaserCrashGame
-    const PhaserCrashGame = dynamic(() => import('../../components/crash/PhaserCrashGame'), { ssr: false });
-
-    export default function ClientCrashWrapper() {
-      return <PhaserCrashGame />;
-    }
-    
-
+  
