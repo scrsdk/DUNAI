6cb7053858fb805fe90366255aa415f6db92e648
KirScr <142673005+scrsdk@users.noreply.github.com>
2025-09-07 23:52:14 +0300

    Update page.tsx

--- app/crash/page.tsx
+++ app/crash/page.tsx
@@ -1,10 +1,13 @@
-import ClientCrashWrapper from './ClientCrashWrapper';
+"use client"
 
-    export default function CrashPage() {
-      return (
-        <div className="w-full h-full min-h-screen bg-black flex items-center justify-center">
-          {/* Теперь используйте вашу обертку */}
-          <ClientCrashWrapper />
-        </div>
-      );
-    }
+import dynamic from 'next/dynamic';
+
+const PhaserCrashGame = dynamic(() => import('../../components/crash/PhaserCrashGame'), { ssr: false });
+
+export default function CrashPage() {
+  return (
+    <div className="w-full h-full min-h-screen bg-black flex items-center justify-center">
+      <PhaserCrashGame />
+    </div>
+  );
+} 
